a22eea45ed577a5846868417e8f6989a163d3f9c
M4rFri <MarcelF@tessonics.com>
2025-12-03 15:55:24 +0100

    added additional options / settings

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -4,17 +4,37 @@
 # Arguments:
 #   target_name: The name of the target for which the version header will be generated.
 function(make_version target_name)
-    include(GetGitVersion)
-    if(GIT_VER_SEM)
+    set(options FORCE_RUN_GETGITVERSION DISABLE_INCLUDE_DIRECTORY)
+    set(oneValueArgs OUTPUT_FOLDER OUTPUT_FILE)
+    cmake_parse_arguments(_make_version "${options}" "${oneValueArgs}" "" ${ARGN})
+
+    if(NOT GIT_VER_SEM OR _make_version_FORCE_RUN_GETGITVERSION)
+        include(GetGitVersion)
         get_git_version_info()
     endif()
+
+    if(_make_version_OUTPUT_FOLDER)
+        set(_output_folder "${_make_version_OUTPUT_FOLDER}")
+    else()
+        set(_output_folder "${CMAKE_CURRENT_BINARY_DIR}")
+    endif()
+
+    if(_make_version_OUTPUT_FILE)
+        set(_output_file "${_make_version_OUTPUT_FILE}")
+    else()
+        set(_output_file "${target_name}_version.h")
+    endif()
+
+
     message(STATUS "${target_name}: Generating version header")
     configure_file(
         "${__CMAKE_SCRIPTS_MAKE_VERSION_FOLDER_DIR}/../version.h.in"
-        "${CMAKE_CURRENT_BINARY_DIR}/${target_name}_version.h"
+        "${_output_folder}/${_output_file}"
     )
 
-    target_include_directories(${target_name} PUBLIC "${CMAKE_CURRENT_BINARY_DIR}")
+    if(NOT _make_version_DISABLE_INCLUDE_DIRECTORY)
+        target_include_directories(${target_name} PUBLIC "${CMAKE_CURRENT_BINARY_DIR}")
+    endif()
     message(STATUS "${target_name}: product version ${GIT_VER_SEMANTIC}")
 endfunction()
 
