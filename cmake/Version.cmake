c6bbbe205fb8e7addc7f3367a2af3543d73ac35a
M4rFri <MarcelF@tessonics.com>
2025-12-03 15:19:24 +0100

    call get git version

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -5,6 +5,9 @@
 #   target_name: The name of the target for which the version header will be generated.
 function(make_version target_name)
     include(GetGitVersion)
+    if(GIT_VER_SEM)
+        get_git_version_info()
+    endif()
     message(STATUS "${target_name}: Generating version header")
     configure_file(
         "${__CMAKE_SCRIPTS_MAKE_VERSION_FOLDER_DIR}/../version.h.in"
