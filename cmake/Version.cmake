55bcf585f03128841fd22ae9e4d0f8f2e789df6f
M4rFri <MarcelF@tessonics.com>
2025-12-03 15:16:46 +0100

    getting correct path

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -7,10 +7,12 @@ function(make_version target_name)
     include(GetGitVersion)
     message(STATUS "${target_name}: Generating version header")
     configure_file(
-        "${CMAKE_CURRENT_SOURCE_DIR}/version.h.in"
+        "${__CMAKE_SCRIPTS_MAKE_VERSION_FOLDER_DIR}/../version.h.in"
         "${CMAKE_CURRENT_BINARY_DIR}/${target_name}_version.h"
     )
 
     target_include_directories(${target_name} PUBLIC "${CMAKE_CURRENT_BINARY_DIR}")
     message(STATUS "${target_name}: product version ${GIT_VER_SEMANTIC}")
 endfunction()
+
+set(__CMAKE_SCRIPTS_MAKE_VERSION_FOLDER_DIR "${CMAKE_CURRENT_LIST_DIR}")
